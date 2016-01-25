2abdc10db796a32278064771f11edb0128e990be
sy2222lxy <sy2222lxy@126.com>
2016-01-25 18:03:26 +0800

    Change-Id: I880ba84df42f98e116c346944f2a2ced48dfec02 Signed-off-by: sy2222lxy <sy2222lxy@126.com>

--- src/main/java/org/springframework/beans/factory/config/ConfigurableListableBeanFactory.java
+++ src/main/java/org/springframework/beans/factory/config/ConfigurableListableBeanFactory.java
@@ -1,8 +1,27 @@
 package org.springframework.beans.factory.config;
 
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 
 public interface ConfigurableListableBeanFactory
 		extends ListableBeanFactory, AutowireCapableBeanFactory, ConfigurableBeanFactory {
-
+	
+	void ingnoreDependencyType(Class<?> type);
+	
+	void ignoreDependencyInterface(Class<?> ifc);
+	
+	void registerResolvableDependency(Class<?> dependencyType, Object autowiredValue);
+	
+	boolean isAutowireCandidate(String beanName, DependencyDescriptor descriptor)
+			throws NoSuchBeanDefinitionException;
+	
+	BeanDefinition getBeanDefinition(String beanName) throws NoSuchBeanDefinitionException;
+	
+	void freezeConfiguration();
+	
+	boolean isConfigurationFrozen();
+	
+	void preInstantiateSingletons() throws BeansException;
+	
 }
